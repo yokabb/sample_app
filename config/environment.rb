b352ffdf59a5ff539d7cad819db6c89532caf7a9
Yoshitake Okada <yokada@win-works.com>
2014-05-28 10:47:54 +0900

    Finish user signup

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 SampleApp::Application.initialize!
+
+# bcrypt'のコスト関数を下げることでテストの速度を向上させる。
+  ActiveModel::SecurePassword.min_cost = true
+
+
+
